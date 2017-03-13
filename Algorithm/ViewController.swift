3400806455973fa34c3087c1c2de3714e39f0ff2
IvanLin <lyfne@me.com>
2017-03-13 16:15:07 +0800

    Initial Commit

---
+++ Algorithm/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Algorithm
+//
+//  Created by 林逸凡 on 2017/3/13.
+//  Copyright © 2017年 Ivan.lin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
