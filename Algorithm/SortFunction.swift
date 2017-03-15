dfc96f9839bd7ca8376567423062b9044bb67ce0
IvanLin <lyfne@me.com>
2017-03-15 18:08:55 +0800

    CocktailSort and Insertion Sort

--- Algorithm/SortFunction.swift
+++ Algorithm/SortFunction.swift
@@ -16,20 +16,95 @@ func swap( array: inout Array<Int>, left: Int, right: Int) {
     array[right] = temp
 }
 
-// Bubble Sort
+// Bubble Sort  -->
 
 func bubbleSort(array: inout Array<Int>) {
-    if (array.isEmpty) {
+    if (array.count < 2) {
         return
     }
     
-    for startIndex in 0..<array.count-1 {  //Here ... means <=, ..< means <
-        for index in 0..<array.count-startIndex-1 {
+    var mainOpeartionTimes = 0
+    
+    //Here ... means <=, ..< means <
+    for startIndex in 0...array.count-2 {
+        mainOpeartionTimes += 1
+        for index in 0...array.count-startIndex-2 {
+            mainOpeartionTimes += 1
             if array[index] > array[index+1] {
                 swap(array: &array, left: index, right: index+1)
             }
         }
     }
+    
+    print("BubbleSort Main Operation Times: " + String(mainOpeartionTimes))
+    print(array, "\n")
+}
+
+// Cocktail Sort, an improvement of Bubble Sort  --> & <--
+
+func cocktailSort(array: inout Array<Int>) {
+    if (array.count < 2) {
+        return
+    }
+    
+    var mainOpeartionTimes = 0
+    var left = 0
+    var right = array.count - 1
+    
+    while left < right {
+        for indexL in left...right-1 {
+            mainOpeartionTimes += 1
+            if array[indexL] > array[indexL+1] {
+                swap(array: &array, left: indexL, right: indexL+1)
+            }
+        }
+        right -= 1
+        for indexR in stride(from: right, through: left+1, by: -1) {
+            mainOpeartionTimes += 1
+            if array[indexR] < array[indexR-1] {
+                swap(array: &array, left: indexR, right: indexR-1)
+            }
+        }
+        left += 1
+    }
+    
+    
+    print("CocktailSort Main Operation Times: " + String(mainOpeartionTimes))
+    print(array, "\n")
+}
+
+// Insertion Sort
+
+func insertionSort(array: inout Array<Int>) {
+    if (array.count < 2) {
+        return
+    }
+    
+    var mainOpeartionTimes = 0
+    
+    for index in 1...array.count-1 {
+        mainOpeartionTimes += 1
+        for backIndex in 0...index-1 {
+            mainOpeartionTimes += 1
+            if array[index]<array[backIndex] {
+                let tmp = array[index]
+                for i in stride(from: index, through: backIndex+1, by: -1) {  // move data
+                    array[i] = array[i-1]
+                }
+                array[backIndex] = tmp  // insert
+                break
+            }
+        }
+    }
+    
+    print("InsertionSort Main Operation Times: " + String(mainOpeartionTimes))
+    print(array, "\n")
+}
+
+// Merge Sort, use Divide and Conquer
+
+func mergeSort(array: inout Array<Int>) {
+    
 }
 
 // Quick Sort
--- Algorithm/ViewController.swift
+++ Algorithm/ViewController.swift
@@ -13,9 +13,15 @@ class ViewController: NSViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        var array = [3, 4, 7, 1, 10, 6, 2, 19, 5, 34, 78, 45, 23, 42, 109, 984, 234, 567, 342, 10924]
-        bubbleSort(array: &array)
-        print(array)
+        let array = [3, 4, 7, 1, 10, 6, 2, 19, 5, 34, 78, 45, 23, 42, 109, 984, 234]//, 567, 342, 10924, 2345, 542, 5069, 40987,8745, 4, 3, 56, 987]
+        
+        var array1 = array
+        var array2 = array
+        var array3 = array
+        
+        bubbleSort(array: &array1)
+        insertionSort(array: &array2)
+        cocktailSort(array: &array3)
     }
 
     override var representedObject: Any? {
